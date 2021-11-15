55cf3145b4fa39e8ccab8912b9a22907eb8b035f
farshadHP <haftzendegi@gmail.com>
2021-11-16 03:00:29 +0330

    dataBinding coding - have error

--- MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ViewPagerAdaptor.kt
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ViewPagerAdaptor.kt
@@ -3,29 +3,40 @@ package com.farshadhp.myappvp2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import androidx.databinding.DataBindingUtil
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_vp2.view.*
+import com.farshadhp.myappvp2.databinding.ItemVp2Binding
+import java.util.zip.Inflater
 
 class ViewPagerAdaptor(
+
     val images : List<Int>,
     val imageTexts : List<String>
 ) : RecyclerView.Adapter<ViewPagerAdaptor.ViewPagerViewHolder>(){
     inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    private lateinit var binding: ItemVp2Binding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vp2,parent,false)
-        return  ViewPagerViewHolder(view)
+        //val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vp2,parent,false)
+        binding = ItemVp2Binding.inflate(LayoutInflater.from(parent.context))
+        return  ViewPagerViewHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
         val curImage = images[position]
         val curImageText = imageTexts[position]
-        holder.itemView.ivImage.setImageResource(curImage)
-        holder.itemView.tvText.setText(curImageText)
+        binding.ivImage.setImageResource(curImage)
+        binding.tvText.setText(curImageText)
     }
 
     override fun getItemCount(): Int {
         return  images.size
     }
 }
+
+//binding = DataBindingUtil.setContentView(,R.layout.item_vp2)
+//ItemVp2Binding.inflate(LayoutInflater.from())
+
+//val binding = DataBindingUtil.setContentView(,R.layout.item_vp2)
