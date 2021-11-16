e7491683ee1172cacdb322e9c6852743e86194d4
farshadHP <haftzendegi@gmail.com>
2021-11-16 17:15:46 +0330

    dataBinding completed no error

--- MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ViewPagerAdaptor.kt
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ViewPagerAdaptor.kt
@@ -1,42 +1,29 @@
 package com.farshadhp.myappvp2
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-
-import androidx.databinding.DataBindingUtil
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.farshadhp.myappvp2.databinding.ItemVp2Binding
-import java.util.zip.Inflater
 
 class ViewPagerAdaptor(
 
     val images : List<Int>,
     val imageTexts : List<String>
 ) : RecyclerView.Adapter<ViewPagerAdaptor.ViewPagerViewHolder>(){
-    inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    private lateinit var binding: ItemVp2Binding
+    inner class ViewPagerViewHolder(val viewDataBinding: ItemVp2Binding) : RecyclerView.ViewHolder(viewDataBinding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
-        //val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vp2,parent,false)
-        binding = ItemVp2Binding.inflate(LayoutInflater.from(parent.context))
-        return  ViewPagerViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdaptor.ViewPagerViewHolder {
+        val binding  =  ItemVp2Binding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return  ViewPagerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
-        val curImage = images[position]
-        val curImageText = imageTexts[position]
-        binding.ivImage.setImageResource(curImage)
-        binding.tvText.setText(curImageText)
+        holder.viewDataBinding.ivImage.setImageResource(images[position])
+        holder.viewDataBinding.tvText.text = imageTexts[position]
+
     }
 
     override fun getItemCount(): Int {
         return  images.size
     }
 }
-
-//binding = DataBindingUtil.setContentView(,R.layout.item_vp2)
-//ItemVp2Binding.inflate(LayoutInflater.from())
-
-//val binding = DataBindingUtil.setContentView(,R.layout.item_vp2)
