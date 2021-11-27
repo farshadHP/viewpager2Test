5bbff6094801c5abac1188917c3901acef8e35cd
farshadHP <haftzendegi@gmail.com>
2021-11-27 18:48:00 +0330

    solve some error and API Works Fine

--- MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/MainActivity.kt
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/MainActivity.kt
@@ -1,39 +1,38 @@
 package com.farshadhp.myappvp2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.farshadhp.myappvp2.Model.Model
+import com.farshadhp.myappvp2.Model.MyViewModel
+import com.farshadhp.myappvp2.Model.ViewModelFactory
+import com.farshadhp.myappvp2.Repository.Repository
 import com.farshadhp.myappvp2.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var viewModel : MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        val images = listOf(
-//            listOf(R.drawable.rrcullinan,R.drawable.rrphantom,R.drawable.rrghost),
-//            listOf(R.drawable.bmw1,R.drawable.bmw2,R.drawable.bmw3)
-//        )
-//        val imageTexts = listOf(
-//            listOf("RollsRoyce Cullinan","RollsRoyce Phantom","RollsRoyce Ghost"),
-//            listOf("BMW X3","BMW M3","BMW M5")
-//
- //       )
-        val images = listOf(
-            Model("RollsRoyce Cullinan",R.drawable.rrcullinan),
-            Model("RollsRoyce Cullinan",R.drawable.rrcullinan),
-            Model("RollsRoyce Cullinan",R.drawable.rrcullinan),
 
-        )
-        val adapter = RecyclerViewAdaptor(images,imageTexts)
+        val repository = Repository()
+        val viewModelFactory = ViewModelFactory(repository)
+        viewModel = ViewModelProvider(this,viewModelFactory).get(MyViewModel::class.java)
+        viewModel.getModel()
+        viewModel.myResponse.observe(this, Observer { response ->
+            Log.e("Response", response.toString())
+
+        })
+        //val adapter = RecyclerViewAdaptor(images,imageTexts)
         //binding.vp.layoutManager()
-        binding.rv.layoutManager = LinearLayoutManager(this)
+        //binding.rv.layoutManager = LinearLayoutManager(this)
                 //GridLayoutManager(this, 2)
-        binding.rv.adapter = adapter
+        //binding.rv.adapter = adapter
 
     }
 }
