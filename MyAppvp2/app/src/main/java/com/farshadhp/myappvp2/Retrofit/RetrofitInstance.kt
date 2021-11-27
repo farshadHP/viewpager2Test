196f7dde7a01fa60f51cbee584798db597ae3f20
farshadHP <haftzendegi@gmail.com>
2021-11-27 15:15:15 +0330

    Add retrofitInstance class

---
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/Retrofit/RetrofitInstance.kt
@@ -0,0 +1,18 @@
+package com.farshadhp.myappvp2.Retrofit
+
+import com.farshadhp.myappvp2.Retrofit.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: SimpleApi by lazy {
+        retrofit.create(SimpleApi::class.java)
+    }
+}
