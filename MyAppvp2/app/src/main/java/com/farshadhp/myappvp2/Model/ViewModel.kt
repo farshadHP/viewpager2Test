0f62cb129d8e1140dae2adaebd3c86970ada6f1e
farshadHP <haftzendegi@gmail.com>
2021-11-27 15:28:59 +0330

    Complete ViewModel class and Add one Dependency

--- MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/Model/ViewModel.kt
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/Model/ViewModel.kt
@@ -1,4 +1,18 @@
 package com.farshadhp.myappvp2.Model
 
-class ViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.farshadhp.myappvp2.Repository.Repository
+import kotlinx.coroutines.launch
+
+class ViewModel(private  val repository: Repository): ViewModel() {
+    var myResponse: MutableLiveData<Model> = MutableLiveData()
+
+    fun getModel(){
+        viewModelScope.launch {
+            val response:Model = repository.getModel()
+            myResponse.value= response
+        }
+    }
 }
