8c5ef1015688d3c3a263cd4178a5a1102bd67fd8
farshadHP <haftzendegi@gmail.com>
2021-12-01 16:40:52 +0330

    loading dialog added and code is completed

--- MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ItemDetailActivity.kt
+++ MyAppvp2/app/src/main/java/com/farshadhp/myappvp2/ItemDetailActivity.kt
@@ -1,7 +1,9 @@
 package com.farshadhp.myappvp2
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.Observer
@@ -10,10 +12,11 @@ import com.farshadhp.myappvp2.Model.MyViewModel
 import com.farshadhp.myappvp2.Model.ViewModelFactory
 import com.farshadhp.myappvp2.Repository.Repository
 import com.farshadhp.myappvp2.databinding.ActivityItemDetailBinding
+import com.farshadhp.myappvp2.databinding.LoadingDialogBinding
 
 class ItemDetailActivity : AppCompatActivity() {
     private  lateinit var viewModel : MyViewModel
-
+    private lateinit var  loadingDialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityItemDetailBinding = ActivityItemDetailBinding.inflate(layoutInflater)
@@ -25,6 +28,15 @@ class ItemDetailActivity : AppCompatActivity() {
         viewModel.getModel(1, intent.extras?.getInt("id")!!)
         //viewModel.getCustomModel(1)
         //Toast.makeText(this,viewModel.myResponse.toString(),Toast.LENGTH_LONG).show()
+        loadingDialog = Dialog(this)
+        val dialogBinding : LoadingDialogBinding = LoadingDialogBinding.inflate(layoutInflater)
+        loadingDialog.setContentView(dialogBinding.root)
+        loadingDialog.show()
+        loadingDialog.setCancelable(false)
+        //val handler : Handler
+        Handler().postDelayed({
+            loadingDialog.dismiss()
+        },1000)
         viewModel.myResponse.observe(this, Observer { response ->
             if(response.isSuccessful){
                 //Log.e("hellllllllllllllllo","\n\n\n\n\n\n\n\n hello \n\n\n")
@@ -37,6 +49,7 @@ class ItemDetailActivity : AppCompatActivity() {
                 Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
             }
         })
+
         //Toast.makeText(this,intent.extras?.getInt("id").toString(),Toast.LENGTH_LONG).show()
 
     }
